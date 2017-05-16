bf0f4996a8c5aac0ace0289556ad60ce4e590fd0
Steve Gredell <steve.gredell@milliman.com>
2017-05-16 12:03:40 -0400

    Move logic into a method
    Do this to productionize the checklist-parser code

--- Testcase_Builder/testcase_parser.py
+++ Testcase_Builder/testcase_parser.py
@@ -9,54 +9,54 @@ from pathlib import Path
 checklist = Path(r"C:\Users\steve.gredell\repos\analytics-pipeline\800_QlikView_QVWs\_Checklists\PRCA_Report_Checklist.txt")
 xml_out = Path(r"C:\Users\steve.gredell\repos\analytics-pipeline\800_QlikView_QVWs\_Checklists\PRCA_Report_Checklist.xml")
 
-# root = ET.Element("suite")
-# name = ET.SubElement(root, "name")
-# name.text = "Care Coordinator Report"
-# desc = ET.SubElement(root, "description")
-sections = ET.Element("sections")
-step_index = 1
-with checklist.open() as cl:
-    for line in cl:
-        if line[0] == '!':
-            section = ET.SubElement(sections, "section")
-            sec_name = ET.SubElement(section, "name")
-            sec_name.text = line[1:].rstrip()
-            ET.SubElement(section, "description")
-            cases = ET.SubElement(section, "cases")
-        elif line[0] == '|':
-            step = ET.SubElement(steps_separated, "step")
-            index = ET.SubElement(step, "index")
-            index.text = str(step_index)
-            step_index += 1
-            ET.SubElement(step, "content")
-            expected = ET.SubElement(step, "expected")
-            expected.text = line[1:].rstrip()
-        elif line[0] == '(':
-            pass  # ignore comment line
-        elif line[0] == '\n':
-            pass  # ignore blank lines
-        elif line[0] == '*':
-            # desc.text = "".join(['*', line[2:].rstrip()])
-            pass
-        elif line[0:2] == '-|':
-            # This is a SQL statement, put it in the "prerequisites" section
-            preconds.text = "".join(["`", line[2:].replace('^nl', '\n').rstrip(), "`"])
-        else:
-            case = ET.SubElement(cases, "case")
-            ET.SubElement(case, "id")
-            title = ET.SubElement(case, "title")
-            title.text = line.rstrip()
-            template = ET.SubElement(case, "template")
-            template.text = "Test Case (Steps)"
-            ET.SubElement(case, "type")
-            ET.SubElement(case, "priority")
-            ET.SubElement(case, "estimate")
-            ET.SubElement(case, "milestone")
-            ET.SubElement(case, "references")
-            custom = ET.SubElement(case, "custom")
-            preconds = ET.SubElement(custom, "preconds")
-            steps_separated = ET.SubElement(custom, "steps_separated")
-            step_index = 1  # reset step index when creating a new test case
 
-tree = ET.ElementTree(sections)
-tree.write(str(xml_out))
+
+def build_testcase_xml(path_checklist: Path, path_xml: Path) -> None:
+
+    sections = ET.Element("sections")
+    step_index = 1
+    with path_checklist.open() as cl:
+        for line in cl:
+            if line[0] == '!':
+                section = ET.SubElement(sections, "section")
+                sec_name = ET.SubElement(section, "name")
+                sec_name.text = line[1:].rstrip()
+                ET.SubElement(section, "description")
+                cases = ET.SubElement(section, "cases")
+            elif line[0] == '|':
+                step = ET.SubElement(steps_separated, "step")
+                index = ET.SubElement(step, "index")
+                index.text = str(step_index)
+                step_index += 1
+                ET.SubElement(step, "content")
+                expected = ET.SubElement(step, "expected")
+                expected.text = line[1:].rstrip()
+            elif line[0] == '(':
+                pass  # ignore comment line
+            elif line[0] == '\n':
+                pass  # ignore blank lines
+            elif line[0] == '*':
+                # ignore comments
+                pass
+            elif line[0:2] == '-|':
+                # This is a SQL statement, put it in the "prerequisites" section
+                preconds.text = "".join(["`", line[2:].replace('^nl', '\n').rstrip(), "`"])
+            else:
+                case = ET.SubElement(cases, "case")
+                ET.SubElement(case, "id")
+                title = ET.SubElement(case, "title")
+                title.text = line.rstrip()
+                template = ET.SubElement(case, "template")
+                template.text = "Test Case (Steps)"
+                ET.SubElement(case, "type")
+                ET.SubElement(case, "priority")
+                ET.SubElement(case, "estimate")
+                ET.SubElement(case, "milestone")
+                ET.SubElement(case, "references")
+                custom = ET.SubElement(case, "custom")
+                preconds = ET.SubElement(custom, "preconds")
+                steps_separated = ET.SubElement(custom, "steps_separated")
+                step_index = 1  # reset step index when creating a new test case
+
+    tree = ET.ElementTree(sections)
+    tree.write(str(path_xml))
